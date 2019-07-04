f04d52f08843ef86604c9332e783974e615898f2
AcostaErk <acostaerk@gmail.com>
2019-07-03 23:59:55 -0600

    Prueba Conexion BD Remota

--- html/Conexion.php
+++ html/Conexion.php
@@ -1,6 +1,6 @@
 <?php
 try{
-	$conn = new PDO('mysql:host=localhost:3306;dbname=Gestion_Plan_Desarrollo;chartset=gbk',  "root", "root");
+	$conn = new PDO('mysql:host=ahuacatlan.serviciosmunicipales.com.mx; dbname=servi301_Gestion_Plan_Desarrollo; chartset=gbk',  "servi301_root", "msBKy#@,SZ{1");
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
