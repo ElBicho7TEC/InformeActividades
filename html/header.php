96849c25d96ac666f35673313417bd7745394b67
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-07-06 11:16:18 -0500

    Edición de cabecera añadiendo logo, y carpeta de logos

--- html/header.php
+++ html/header.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="es">
+  <head>
+      <title>
+        Inicio Administrador
+      </title>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Anton|Dosis:400,800" rel="stylesheet">
+      <link rel="stylesheet" href="css/styles.css">
+      <link rel="shortcut icon" href="logos/AhucatlanTransparente.png" type="image/png" /> <!--Se agrega el icono a la cabecera de la página desde la ubicación de la carpeta logos-->
+  </head>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Anton|Dosis:400,800" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-
-<body>
-    <header class="bg-light container-fluid">
-      <div class="row justify-content-center text-center text-md-left">
+  <body >
+      <header class="bg-light container-fluid" style="background:url(logos/cabecera.png) no-repeat; height:100px; background-repeat: no-repeat; background-color: white; background-attachment: fixed; background-position: top center; background-size: auto; position: relative;">
+        <div class="row justify-content-center text-center text-md-left" style="background-color: white;">
           <div class="col-12 col-md-4 mt-4 mt-md-0">
-Encabezado de pagina
-      </div>
-    </div>
-    </header>
-    <?php
- ?>
+            <!--Texto que irá en el encabezado de la página -->
+          </div>
+        </div>
+      </header>
