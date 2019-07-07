261aa10ac83c22082d255a409ea392b653fbfa03
Samuel <samuelmojarroornelas1997@gmail.com>
2019-07-07 11:51:51 -0600

    Base de datos en minusculas

--- bd/BD_Gestion_Plan_Desarrollo3.sql
+++ bd/BD_Gestion_Plan_Desarrollo3.sql
@@ -1,94 +1,94 @@
-CREATE DATABASE Gestion_Plan_Desarrollo;
-USE Gestion_Plan_Desarrollo;
+CREATE DATABASE gestion_plan_desarrollo;
+USE gestion_plan_desarrollo;
 
--- -----------------------------------------------------
+-- ------------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`usuarios`
 -- -----------------------------------------------------
 CREATE TABLE usuarios (
-  idUsuario INT NOT NULL AUTO_INCREMENT,
-  correoElectronico VARCHAR(45) NOT NULL,
+  idusuario INT NOT NULL AUTO_INCREMENT,
+  correoelectronico VARCHAR(45) NOT NULL,
   contrasena VARCHAR(200) NOT NULL,
-  Nombre VARCHAR(45) NOT NULL,
-  apellidoPaterno VARCHAR(45) NOT NULL,
-  apellidoMaterno VARCHAR(45) NOT NULL,
-  idDependencia INT NOT NULL,
-  idRol INT NOT NULL,
-  PRIMARY KEY ( idUsuario, correoElectronico)
+  nombre VARCHAR(45) NOT NULL,
+  apellidopaterno VARCHAR(45) NOT NULL,
+  apellidomaterno VARCHAR(45) NOT NULL,
+  iddependencia INT NOT NULL,
+  idrol INT NOT NULL,
+  PRIMARY KEY ( idusuario, correoelectronico)
 );
 
 -- -----------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`rol`
 -- -----------------------------------------------------
 CREATE TABLE rol(
-  idRol INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  tipoRol VARCHAR(45) NOT NULL
+  idrol INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tiporol VARCHAR(45) NOT NULL
 );
 
 -- -----------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`dependencia`
 -- -----------------------------------------------------
 CREATE TABLE dependencia(
-  idDependencia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nombreDeependencia VARCHAR(45) NOT NULL,
+  iddependencia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombredependencia VARCHAR(45) NOT NULL,
   calle VARCHAR(45) NOT NULL,
   colonia VARCHAR(45) NOT NULL,
   numero VARCHAR(45) NOT NULL,
-  codigoPostal VARCHAR(45) NOT NULL,
-  entreCalles VARCHAR(45) NOT NULL
+  codigopostal VARCHAR(45) NOT NULL,
+  entrecalles VARCHAR(45) NOT NULL
 );
 
 -- -----------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`planDesarrollo`
 -- -----------------------------------------------------
-CREATE TABLE planDesarrollo (
-  idplanDesarrollo INT NOT NULL PRIMARY KEY,
-  nombrePlanDesarrollo VARCHAR(45) NOT NULL,
+CREATE TABLE plandesarrollo (
+  idplandesarrollo INT NOT NULL PRIMARY KEY,
+  nombreplandesarrollo VARCHAR(45) NOT NULL,
   descripcion VARCHAR(45) NOT NULL,
-  Estatus VARCHAR(45) NOT NULL,
-  fechaProgramada DATETIME NOT NULL,
-  idDependencia INT NOT NULL
+  estatus VARCHAR(45) NOT NULL,
+  fechaprogramada DATETIME NOT NULL,
+  iddependencia INT NOT NULL
 );
 
 -- -----------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`informe`
 -- -----------------------------------------------------
 CREATE TABLE informe(
-  idInforme INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  notaEvidencia VARCHAR(45) NOT NULL,
+  idinforme INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  notaevidencia VARCHAR(45) NOT NULL,
   fecha DATETIME NOT NULL,
-  idplanDesarrollo INT NOT NULL
+  idplandesarrollo INT NOT NULL
 );
 
 -- -----------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`fotosEvidencia`
 -- -----------------------------------------------------
-CREATE TABLE fotosEvidencia(
-  idfotosEvidencia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE fotosevidencia(
+  idfotosevidencia INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   ruta VARCHAR(200) NOT NULL,
-  idInforme INT NOT NULL
+  idinforme INT NOT NULL
 );
 -- -----------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`actividad`
 -- -----------------------------------------------------
 CREATE TABLE actividad(
-  idActividad INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  nombre_Actividad VARCHAR(500) NOT NULL,
+  idactividad INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre_actividad VARCHAR(500) NOT NULL,
   descripcion VARCHAR(500) NOT NULL,
-  fechaDia INT NOT NULL,
-  fechaMes INT NOT NULL,
-  fechaAno INT NOT NULL,
-  Bimestre INT NOT NULL,
-  idDependencia INT NOT NULL,
-  idfotosEvidencia INT NOT NULL,
-  idplanDesarrollo INT NOT NULL
+  fechadia INT NOT NULL,
+  fechames INT NOT NULL,
+  fechaano INT NOT NULL,
+  bimestre INT NOT NULL,
+  iddependencia INT NOT NULL,
+  idfotosevidencia INT NOT NULL,
+  idplandesarrollo INT NOT NULL
 );
 
 -- -----------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`tokenSesion`
 -- -----------------------------------------------------
-CREATE TABLE tokenSesion(
+CREATE TABLE tokensesion(
   token varchar(45) NOT NULL PRIMARY KEY,
-  idUsuario INT NOT NULL,
+  idusuario INT NOT NULL,
   expira DATE NOT NULL
 );
 
@@ -96,20 +96,20 @@ CREATE TABLE tokenSesion(
 -- -----------------------------------------------------
 -- Table `Gestion_Plan_Desarrollo`.`Historial_Actividad`
 -- -----------------------------------------------------
-CREATE TABLE Historial_Actividad(
-  idUsuario INT NOT NULL, 
-  idActividad INT NOT NULL ,
-  fechaDia INT NOT NULL,
-  fechaMes INT NOT NULL,
-  fechaAno INT NOT NULL,
-  Descripción varchar(500) not null
+CREATE TABLE historial_actividad(
+  idusuario INT NOT NULL, 
+  idactividad INT NOT NULL ,
+  fechadia INT NOT NULL,
+  fechames INT NOT NULL,
+  fechaano INT NOT NULL,
+  descripción varchar(500) not null
 );
 
 #Procedimiento almacenado para modificar Roles
 DELIMITER //
-create procedure sp_modificarRoles(var_idRol INT,var_tipoRol VARCHAR(45))
+create procedure sp_modificarroles(var_idrol INT,var_tiporol VARCHAR(45))
 begin
-UPDATE rol SET tipoRol=var_tipoRol WHERE idRol = var_idRol;
+UPDATE rol SET tiporol=var_tiporol WHERE idrol = var_idrol;
 end
 ;
 //
@@ -117,35 +117,35 @@ DELIMITER ;
 
 #Procedimiento almacenado para modificar Dependencias
 DELIMITER //
-create procedure sp_modificarDependencias(
-	 var_idDependencia INT,
-	 var_nombreDeependencia VARCHAR(45),
+create procedure sp_modificardependencias(
+	 var_iddependencia INT,
+	 var_nombredependencia VARCHAR(45),
 	 var_calle VARCHAR(45),
 	 var_colonia VARCHAR(45),
 	 var_numero VARCHAR(45),
-	 var_codigoPostal VARCHAR(45),
-	 var_entreCalles VARCHAR(45)
+	 var_codigopostal VARCHAR(45),
+	 var_entrecalles VARCHAR(45)
 )
 BEGIN
     UPDATE dependencia SET 
-    nombreDeependencia=var_nombreDeependencia,
+    nombredeependencia=var_nombredeependencia,
     calle=var_calle,
     colonia=var_colonia,
     numero=var_numero,
-    codigoPostal=var_codigoPostal,
-    entreCalles=var_entreCalles
-    WHERE idDependencia=var_idDependencia;
+    codigopostal=var_codigopostal,
+    entrecalles=var_entrecalles
+    WHERE iddependencia=var_iddependencia;
 END;
 //
 DELIMITER ;
 
 #Procedimiento para insertar Rol
 DELIMITER //
-create procedure sp_insertarRoles(nombreRol varchar(45))
+create procedure sp_insertarroles(nombrerol varchar(45))
 begin
-set @id= (select   idRol from rol ORDER BY idRol DESC limit 1) ;
+set @id= (select   idrol from rol ORDER BY idrol DESC limit 1) ;
 set @id=@id+1;
-insert into rol values(@id,nombreRol);
+insert into rol values(@id,nombrerol);
 end
 ;
 //
@@ -153,13 +153,13 @@ DELIMITER ;
 
 #Procedimiento para insertar Dependencia
 DELIMITER //
-create procedure sp_insertarDependencias(nombreDeependencia VARCHAR(45), calle VARCHAR(45),colonia VARCHAR(45),numero VARCHAR(45),
-codigoPostal VARCHAR(45),entreCalles VARCHAR(45))
+create procedure sp_insertardependencias(nombredependencia VARCHAR(45), calle VARCHAR(45),colonia VARCHAR(45),numero VARCHAR(45),
+codigopostal VARCHAR(45),entrecalles VARCHAR(45))
 begin
-set @id= (select   idDependencia from dependencia ORDER BY idDependencia DESC limit 1) ;
+set @id= (select   iddependencia from dependencia ORDER BY iddependencia DESC limit 1) ;
 set @id=@id+1;
-insert into dependencia values (default, nombreDeependencia, calle, colonia, numero, codigoPostal,
-    entreCalles);
+insert into dependencia values (default, nombredependencia, calle, colonia, numero, codigopostal,
+    entrecalles);
 end
 ;
 //
@@ -167,68 +167,68 @@ DELIMITER ;
 
 #Procedimiento almacenado para insertar Actividades 
 DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_Insertar_Actividades`(
-	in nombre_Actividad VARCHAR(500),
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insertar_actividades`(
+	in nombre_actividad VARCHAR(500),
 	in descripcion VARCHAR(500),
-	in fechaDia INT,
-	in fechaMes INT,
-	in fechaAno INT,
-	in Bimestre INT,
-	in idDependencia INT,
-	in idfotosEvidencia INT,
-	in idplanDesarrollo INT
+	in fechadia INT,
+	in fechames INT,
+	in fechaano INT,
+	in bimestre INT,
+	in iddependencia INT,
+	in idfotosevidencia INT,
+	in idplandesarrollo INT
 )
 BEGIN
-	insert into actividad values (default, nombre_Actividad, descripcion, fechaDia, fechaMes, fechaAno,
-    Bimestre, idDependencia, idfotosEvidencia, idplanDesarrollo);
+	insert into actividad values (default, nombre_actividad, descripcion, fechadia, fechames, fechaano,
+    bimestre, iddependencia, idfotosevidencia, idplandesarrollo);
 END
 //
 DELIMITER ;
 
 DELIMITER //
 #Procedimiento almacenado para insertar Usuarios
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_Insertar_Usuarios`(
-  in correoElectronico VARCHAR(45),
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insertar_usuarios`(
+  in correoelectronico VARCHAR(45),
   in contrasena VARCHAR(200),
-  in Nombre VARCHAR(45),
-  in apellidoPaterno VARCHAR(45),
-  in apellidoMaterno VARCHAR(45),
-  in idDependencia INT,
-  in idRol INT
+  in nombre VARCHAR(45),
+  in apellidopaterno VARCHAR(45),
+  in apellidomaterno VARCHAR(45),
+  in iddependencia INT,
+  in idol INT
 )
 BEGIN
-	insert into usuarios values (default, correoElectronico, contrasena, Nombre,
-    apellidoPaterno, apellidoMaterno, idDependencia, idRol);
+	insert into usuarios values (default, correoelectronico, contrasena, nombre,
+    apellidopaterno, apellidomaterno, iddependencia, idrol);
 END
 //
 DELIMITER ;
 
 DELIMITER //
 #Procedimiento almacenado para modificar actividades 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_Modificar_Actividades`(
-	in idActividad INT,
-	in nombre_Actividad VARCHAR(500),
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_modificar_actividades`(
+	in idactividad INT,
+	in nombre_actividad VARCHAR(500),
 	in descripcion VARCHAR(500),
-	in fechaDia INT,
-	in fechaMes INT,
-	in fechaAno INT,
-	in Bimestre INT,
-	in idDependencia INT,
-	in idfotosEvidencia INT,
-	in idplanDesarrollo INT
+	in fechadia INT,
+	in fechames INT,
+	in fechaano INT,
+	in bimestre INT,
+	in iddependencia INT,
+	in idfotosevidencia INT,
+	in idplandesarrollo INT
 )
 BEGIN
 	 UPDATE actividad SET 
-    nombre_Actividad=nombre_Actividad,
+    nombre_actividad=nombre_actividad,
     descripcion=descripcion,
-    fechaDia=fechaDia,
-    fechaMes=fechaMes,
-    fechaAno=fechaAno,
-    Bimestre=Bimestre,
-    idDependencia=idDependencia,
-    idfotosEvidencia=idfotosEvidencia,
-    idplanDesarrollo=idplanDesarrollo
-    WHERE idActividad = idActividad;
+    fechadia=fechadia,
+    fechames=fechames,
+    fechaano=fechaano,
+    bimestre=bimestre,
+    iddependencia=iddependencia,
+    idfotosevidencia=idfotosevidencia,
+    idplandesarrollo=idplandesarrollo
+    WHERE idactividad = idactividad;
 END
 //
 DELIMITER ;
