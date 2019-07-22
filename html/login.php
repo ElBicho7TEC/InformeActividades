e81190128e917471ba8f2c587b8d1f5a5f841c33
Jose Cordero <josehipolito.c.a@gmail.com>
2019-07-21 23:48:33 -0600

    Update login.php
    Modificacion  pequeños erroes duplicado d3 campos

--- html/login.php
+++ html/login.php
@@ -1,86 +1,46 @@
-<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
-<link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css">
 <link rel="stylesheet" type="text/css" href="css/styles.css">
 <link rel="stylesheet" type="text/css" href="css/login.css">
-<div>
- <?php include 'header.php' ?>
+<?php include 'header.php' ?>
 </div>
-
-
 <?php
-if (isset($_GET["error"])){
-    $idError=$_GET["error"];
-
-
-    if ($idError==2){ //Caso de mal password
-        ?>
-        <div class="container">
-            <div class="alert alert-danger" role="alert">
-                <p>Por favor revise que su correo y contraseña sean correctos</p>
-            </div>
-
+     if (isset($_GET["error"]))
+     {
+      $idError=$_GET["error"];
+     if ($idError==2)
+     { //Caso de mal password
+?>
+ <div class="container">
+     <div class="alert alert-danger" role="alert">
+         <p>Por favor revise que su correo y contraseña sean correctos</p>
+     </div>
+ </div>
+    <?php 
+  }
+    else if ($idError==1) { //Caso de correo inexistente
+    ?>
+    <div class="container">
+        <div class="alert alert-danger" role="alert">
+            <p>El usuario ingresado no existe. Revisa tu correo porfavor</p>
         </div>
+    </div>
     <?php }
-    else    if ($idError==1) { //Caso de correo inexistente
-        ?>
-        <div class="container">
-            <div class="alert alert-danger" role="alert">
-                <p>El usuario ingresado no existe. Revisa tu correo porfavor</p>
-            </div>
-
-        </div>
-
-    <?php }
-} ?>
+    } ?>
 <div class="container" id="maindiv">
   <div class="row justify-content-center text-center text-md-left">
-<<<<<<< HEAD
 	  <div class="col-12 col-md-4 mt-4 mt-md-0">
 	      <form class="Myformulario" action="verificarLogin.php" method="post" enctype="multipart/form-data">
 	          <div class="col-12 form-group">
 	              <label>Correo</label>
 	              <input   type="text" id="correo" name="correo" value="" maxlength="73" pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}" required="" >
-
 	          </div>
 	          <div class="col-12   form-group">
 	              <label>Password</label>
 	              <input type="password" id="pass" name="pass" value="" maxlength="73" required="">
 	          </div>
-
 	          <div class="col-12   form-group ">
 	              <input type="submit"  id="entrar" name="entrar" value="Entrar">
-
 	          </div>
 	     </form>  
 	  </div>
 	</div>
-=======
-  <div class="col-12 col-md-4 mt-4 mt-md-0">
-      <form class="formularioLogin" action="verificarLogin.php" method="post" enctype="multipart/form-data">
-          <div class="col-12 form-group">
-              <label>Correo</label>
-              <input   type="text" id="correo" name="correo" value="" maxlength="73" pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}" required="" >
-
-          </div>
-          <div class="col-12   form-group">
-              <label>Password</label>
-              <input type="password" id="pass" name="pass" value="" maxlength="73" required="">
-          </div>
-
-          <div class="col-12   form-group ">
-              <input type="submit"  id="entrar" name="entrar" value="Entrar">
-
-          </div>
-
-      </form>  </div>
-
-</div>
-
-
->>>>>>> 21663b94a26b0348d316a7c946b9d4c53c00ba56
-</div>
-
-
-
 <?php include 'footer.php' ?>
