5bdc369ba426b84edada9d9775f3408d3c140f84
Jose Cordero <josehipolito.c.a@gmail.com>
2019-07-29 15:07:35 -0600

    borrar menu y actualizar titulos
    -Borrar menu inecesario esta en el header.
    -Modificaciones en los h´s
    -

--- html/altaDependencia.php
+++ html/altaDependencia.php
@@ -3,60 +3,11 @@
 		<title>Alta Dependencia</title>
 			<?php include 'header.php'; ?>
 	</head>
-	
 	<body>
-	<div class="header-bottom">
-	
 	<div class="container">
-	<div class="row justify-content-center text-center text-md-left">
-				<div class="col-12">
-					<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-						<a class="navbar-brand" href="#">Perfil</a>
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon"></span>
-						</button>
-
-						<div class="collapse navbar-collapse" id="navbarSupportedContent">
-							<ul class="navbar-nav mr-auto">
-								<li class="nav-item active">
-									<a class="nav-link" href="indexAdministrador.php">Inicio <span class="sr-only">(current)</span></a>
-								</li>
-     
-								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-										Usuarios
-									</a>
-									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-										<a class="dropdown-item" href="registro.php">Agregar Nuevo</a>
-										<a class="dropdown-item" href="#">Modificar Usuarios</a>
-									</div>
-								</li>
-	  
-								<li class="nav-item dropdown">
-									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-										Dependencias
-									</a>
-									<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-										<a class="dropdown-item" href="dataDependencia.php">Agregar Dependencia</a>
-										<a class="dropdown-item" href="#">Modificar Dependencia</a>
-									</div>
-								</li>
-							</ul>
-							<form class="form-inline my-2 my-lg-0">
-								<input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-								<button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="Buscar" style="border-color: #5DADE2 !important; color: white !important;" >Buscar</button>
-							</form>
-						</div>
-					</nav>
-				</div>
-				
-			</div>
-	</div>
-	</div>
-	
-	<div class="container">
+		<div class="row justify-content-center text-center text-md-left" >
 		<h1 class="text-center">Registro de Dependencias</h1>
-		<div class="col-md-6" >
+		<div class="col-md-12" >
 			<form class="Myformulario" style="margin-top: 150p%; margin-left: 15%;" action="altaDependenciaDB.php" method="post">
 				<div class="form-group">
 					<label>Nombre:</label>
@@ -79,6 +30,7 @@
 			
 		</div>
 	</div>
+</div>
 	<div class="container">
 	<div class="footer" style="color:#fff; background:#34495E;">
 				<p style="text-align: center;"> © 2019 Copyright: Ahuacatlán, Nayarit.</p>
