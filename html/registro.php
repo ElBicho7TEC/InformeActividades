eeb690d81242c25faedadd35cac3275daaaf397c
Jose Cordero <josehipolito.c.a@gmail.com>
2019-07-29 15:12:31 -0600

    actualizacion a registro
    borrar menu inecesario

--- html/registro.php
+++ html/registro.php
@@ -2,54 +2,12 @@
 		<div class="container"> 
 			<div class="row justify-content-center text-center text-md-left">
 				<div class="col-12 " >
-				    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> <!--Comienzo menu de navegación dark--->
-                        <a class="navbar-brand" href="#"> <!--Brand Primer menu--->
-                         Perfil
-                        </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <!--boton decolapso--->
-                        <span class="navbar-toggler-icon"></span> <!--icono del boton de colapso--->
-                        </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent"> <!--elementos de la navegacion--->
-                        <ul class="navbar-nav mr-auto"> <!--ajuste automatico--->
-                            <li class="nav-item active"><!--activacion de los menus del colapso--->
-                            <a class="nav-link" href="indexAdministrador.php">Inicio<span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Usuarios
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                               <a class="dropdown-item" href="registro.php">Agregar Nuevo Usuario</a>
-                               <a class="dropdown-item" href="#">Modificar Usuarios</a>
-                            </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                               Roles
-                               </a>
-                               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                               <a class="dropdown-item" href="dataRol.php">Agregar Nuevo Rol</a>
-                               <a class="dropdown-item" href="#">Modificar Roles</a>
-                               </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                               Dependencias
-                               </a>
-                               <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                               <a class="dropdown-item" href="dataDependencia.php">Agregar Dependencia</a>
-                               <a class="dropdown-item" href="#">Modificar Dependencia</a>
-                               </div>
-                            </li>
-                        </ul>
-                    </div>
-                </nav> <!--Fin  menu de navegación dark--->
 					<form  class="Myformulario" method='POST' >
 					    <div class="form-group text-center">
-						    <h3>Registro del Usuario</h3>
+						    <h2>Registro del Usuario</h2>
 					    </div>
 					    <div class="form-group text-center">
-						    <h2>Datos Personales</h2>
+						    <h3>Datos Personales</h3>
 					    </div>
 					    <div class="form-group text-center">
 						    <span>Nombre:</span>
@@ -64,7 +22,7 @@
 						   <input type="text" id="ApellidoM" name="ApellidoM" required="" pattern="[A-Za-z]+" maxlength="45">
 					   </div>
 					   <div class="form-group text-center">
-						  <h2>Datos de usuario</h2>
+						  <h3>Datos de usuario</h3>
 					  </div>
 					  <div class="form-group text-center">
 						  <span>Correo eletr&oacute;nico:</span>
