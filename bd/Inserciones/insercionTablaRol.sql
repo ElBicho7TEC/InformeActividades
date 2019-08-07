e15c2b13ae941ff7587971c32542383030de95b6
Ramon Diaz <ramondiaz123@yahoo.com>
2019-08-07 15:50:29 -0600

    Agregado apartado de inserciones. Empezando con los roles.

---
+++ bd/Inserciones/insercionTablaRol.sql
@@ -0,0 +1,12 @@
+INSERT INTO `rol` (`idrol`, `tiporol`) VALUES
+(1, 'Administrador');
+
+INSERT INTO `rol` (`idrol`, `tiporol`) VALUES
+(2, 'Contraloría');
+
+
+INSERT INTO `rol` (`idrol`, `tiporol`) VALUES
+(3, 'Direcciones');
+
+INSERT INTO `rol` (`idrol`, `tiporol`) VALUES
+(4, 'Regidores');
