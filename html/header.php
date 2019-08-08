b4a2c8c7c97ee775e2caafa98a0da2f1d0be8e09
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-08-08 13:48:06 -0500

    Commit de código comentado de las imagenes

--- html/header.php
+++ html/header.php
@@ -346,13 +346,76 @@
                     </a>
                   </div>
                 </div>
-<!-- ----------------------------------------------------------------------------------------------------------------->
-                    <?php
-                      include 'menus.php';
-                      menu();
-                     ?>
-                    
-<!--------------------------------------------------------------------------------------------------------- -->
+
+                <div class="nav-collapse collapse">
+                  <div class="menu-menu-principal-container">
+                    <ul id="menu-menu-principal-2" class="nav">
+                      <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-170">
+                        <a href="indexAdministrador.php?pagina=1">
+                          Inicio
+                        </a>
+                      </li>
+
+                      <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children dropdown menu-item-172">
+                        <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
+                          Usuarios
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2226">
+                            <a href="registro.php">
+                              Agregar Nuevo Usuario
+                            </a>
+                          </li>
+                          <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2226">
+                            <a href="#">
+                              Modificar Usuario
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children dropdown menu-item-327">
+                        <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
+                          Roles
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2242">
+                            <a href="altaRol.php">
+                              Agregar Nuevo Rol
+                            </a>
+                          </li>
+                          <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2242">
+                            <a href="#">
+                              Modificar Rol
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                      <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children dropdown menu-item-383">
+                        <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">
+                          Dependencias
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2261">
+                            <a href="altaDependencia.php">
+                              Agregar Nueva Dependencia 
+                            </a>
+                          </li>
+                          <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2793">
+                            <a href="#">
+                              Modificar Dependencia
+                            </a>
+                          </li>
+                        </ul>
+                      </li>
+                       <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-170">
+                           <a href="logout.php">
+                               Logout
+                           </a>
+                       </li>
+                    </ul>
+                  </div>
+                </div>
+                
                 <div class="btn-toggle-nav pull-right">
                   <button type="button" class="btn menu-btn" data-toggle="collapse" data-target=".nav-collapse" title="Abrir el menú" aria-label="Abrir el menú">
                     <span class="icon-bar">
