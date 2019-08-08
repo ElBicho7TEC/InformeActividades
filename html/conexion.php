dbab2c61c87fe35196cbbb27cfd478667af9acc0
RamonDiaz31 <51445378+RamonDiaz31@users.noreply.github.com>
2019-08-08 02:32:08 -0600

    Update conexion.php

--- html/conexion.php
+++ html/conexion.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $conn = new PDO('mysql:host=localhost; dbname=gestion_plan_desarrollo; chartset=gbk',  "root", "");
+    $conn = new PDO('mysql:host=ahuacatlan.serviciosmunicipales.com.mx; dbname=servi301_gestion_plan_desarrollo; chartset=gbk',  "servi301_root", "msBKy#@,SZ{1");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
