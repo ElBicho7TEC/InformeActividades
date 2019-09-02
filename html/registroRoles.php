459fea1fd5e85266ce8091028614596912ca0275
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-09-02 17:13:20 -0500

    Roles en forma de tabla

--- html/registroRoles.php
+++ html/registroRoles.php
@@ -1,45 +1,54 @@
 <?php include 'getInfo.php'; ?>
 <html>
 	<head>
-		<title>Roles Existentes</title>
-			<?php include 'header.php'; ?>
+		<title>
+			Roles Existentes
+		</title>
+		<?php include 'header.php'; ?>
 	</head>
 
 	<body>
-	<div class="header-bottom">
-
-	<div class="container">
-	<div class="row justify-content-center text-center text-md-left">
+		<div class="header-bottom">
+			<div class="container">
+				<div class="row justify-content-center text-center text-md-left">
+				</div>
 			</div>
-	</div>
-	</div>
+		</div>
+		<div class="container mb-2">
+			<h1 class="text-center">
+				Roles Existentes
+			</h1>
+			<div class="row">
+				<table>
+					<tr>
+					  <td>
+					  	<strong>Rol</strong>
+					  </td>
+					</tr>
 
-	<div class="container mb-2">
-		<h1 class="text-center">Roles Existentes</h1>
-	<div class="row">
-	<?php
-include("conexion.php");
+					<?php
+					include("conexion.php");
 
-  $cadena='SELECT *FROM rol';
-    $gsent = $conn->prepare($cadena);
-    $gsent->execute();
-    while ($resultado = $gsent->fetch(PDO::FETCH_ASSOC)) {
+		  			$cadena='SELECT *FROM rol';
+				    $gsent = $conn->prepare($cadena);
+				    $gsent->execute();
+				    while ($resultado = $gsent->fetch(PDO::FETCH_ASSOC)) 
+				    {
+				    	$tiporol=$resultado['tiporol'];
 
-    $tiporol=$resultado['tiporol'];
+					?>
 
-?>
-
-<div class="col-md-4 col-lg-3 ">
-<div class="border border-dark">
-<div class=" text-light text-center mb-2   bg-dark" >
- <h4> <?php echo $tiporol ?> </h4>
-</div>
-
-</div>
-</div>
-</div>
-<?php } ?>
-
+					<tr>
+						<td>
+					 		<?php echo $tiporol ?> 
+					  	</td>
+					</tr>
+					
+				</div>
+					<?php 
+					} 
+					?>
+				</table>
 		</div>
 	</div>
 	<div class="container">
