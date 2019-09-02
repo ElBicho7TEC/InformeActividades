ae7339c150675ce00e7a4f8f6e32bca71a83552a
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-09-02 17:25:40 -0500

    Dependencias en tablas

--- html/registroDependencias.php
+++ html/registroDependencias.php
@@ -1,61 +1,97 @@
 <?php include 'getInfo.php'; ?>
 <html>
 	<head>
-		<title>Dependencias Existentes</title>
-			<?php include 'header.php'; ?>
+		<title>
+			Dependencias Existentes
+		</title>
+		<?php include 'header.php'; ?>
 	</head>
 
 	<body>
-	<div class="header-bottom">
-
-	<div class="container">
-	<div class="row justify-content-center text-center text-md-left">
+		<div class="header-bottom">
+			<div class="container">
+				<div class="row justify-content-center text-center text-md-left">
+				</div>
 			</div>
-	</div>
-	</div>
-
-	<div class="container mb-2">
-		<h1 class="text-center">Dependencias Existentes</h1>
-	<div class="row">
-	<?php
-include("conexion.php");
-
-  $cadena='SELECT *FROM dependencia';
-    $gsent = $conn->prepare($cadena);
-    $gsent->execute();
-    while ($resultado = $gsent->fetch(PDO::FETCH_ASSOC)) {
-
-    $nombreDeependencia=$resultado['nombredependencia'];
-    $calle=$resultado['calle'];
-    $colonia=$resultado['colonia'];
-    $numero=$resultado['numero'];
-    $codigoPostal=$resultado['codigopostal'];
-    $entreCalles=$resultado['entrecalles'];
-
-?>
-
-<div class="col-md-4 col-lg-3 ">
-<div class="border border-dark">
-<div class=" text-light text-center mb-2   bg-dark" >
- <h4> <?php echo $nombreDeependencia ?> </h4>
-<div class=" text-center p-4 bg-light text-dark">
-<p>  <?php  echo   $calle ?>  #<?php echo  $numero  ?></p>
-<p> <?php  echo   $colonia ?> </p>
-<p> <?php  echo   $codigoPostal ?> </p>
-<p>Entre <?php  echo   $entreCalles ?> </p>
-</div>
-
-</div>
-</div>
-</div>
-<?php } ?>
-
 		</div>
-	</div>
-	<div class="container">
-	<div class="footer" style="color:#fff; background:#34495E;">
+		<div class="container mb-2">
+			<h1 class="text-center">Dependencias Existentes</h1>
+			<div class="row">
+
+				<table>
+					<tr>
+					  <td>
+					  	<strong>Dependencia</strong>
+					  </td>
+					 
+					  <td>
+					  	<strong>Calle</strong>
+					  </td>
+						  
+					  <td>
+					  	<strong>Colonia</strong>
+					  </td>
+
+					  <td>
+					  	<strong>N&uacute;mero</strong>
+					  </td>
+
+					  <td>
+					  	<strong>C&oacute;digo postal</strong>
+					  </td>
+
+					  <td>
+					  	<strong>Entre calles</strong>
+					  </td>
+					</tr>
+
+					<?php
+					include("conexion.php");
+
+				  	$cadena='SELECT *FROM dependencia';
+				    $gsent = $conn->prepare($cadena);
+				    $gsent->execute();
+				    while ($resultado = $gsent->fetch(PDO::FETCH_ASSOC)) 
+				    {
+					    $nombreDeependencia=$resultado['nombredependencia'];
+					    $calle=$resultado['calle'];
+					    $colonia=$resultado['colonia'];
+					    $numero=$resultado['numero'];
+					    $codigoPostal=$resultado['codigopostal'];
+					    $entreCalles=$resultado['entrecalles'];
+					?>
+
+					<tr>
+						<td>
+					  		<?php echo $nombreDeependencia ?> 
+						</td>
+					  	<td>
+							<?php  echo   $calle ?> 
+					  	</td>
+						<td>
+					  		#<?php echo  $numero  ?>
+						</td>
+					  	<td>
+							<?php  echo   $colonia ?> 
+						</td>
+						<td>
+							<?php  echo   $codigoPostal ?> 
+						</td>
+						<td>
+							Entre <?php  echo   $entreCalles ?>
+						</td>
+					</tr>			
+					<?php 
+					} 
+					?>
+				</table>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="footer" style="color:#fff; background:#34495E;">
 				<p style="text-align: center;"> © 2019 Copyright: Ahuacatlán, Nayarit.</p>
 			</div>
-	</div>
+		</div>
 	</body>
 </html>
