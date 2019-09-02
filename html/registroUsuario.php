619d1695f829cb95e52d8930259e10d7b80d78f1
Javier-Ramirez <javi.guti.rama@gmail.com>
2019-09-02 17:06:29 -0500

    Usuarios en forma de tabla y no en divs

--- html/registroUsuario.php
+++ html/registroUsuario.php
@@ -2,58 +2,83 @@
 <html>
 	<head>
 		<title>Usuarios Existentes</title>
-			<?php include 'header.php'; ?>
+		<?php include 'header.php'; ?>
 	</head>
 
 	<body>
-	<div class="header-bottom">
-
-	<div class="container">
-	<div class="row justify-content-center text-center text-md-left">
+		<div class="header-bottom">
+			<div class="container">
+				<div class="row justify-content-center text-center text-md-left">
+				</div>
 			</div>
-	</div>
-	</div>
-
-	<div class="container mb-2">
-		<h1 class="text-center">Usuarios Existentes</h1>
-	<div class="row">
-	<?php
-include("conexion.php");
-
-  $cadena='SELECT *FROM usuarios';
-    $gsent = $conn->prepare($cadena);
-    $gsent->execute();
-    while ($resultado = $gsent->fetch(PDO::FETCH_ASSOC)) {
-
-
-    $Correo=$resultado['correoelectronico'];
-    $Nombre=$resultado['nombre'];
-    $Apellidopat=$resultado['apellidopaterno'];
-    $Apellidomat=$resultado['apellidomaterno'];
-
-?>
-
-<div class="col-md-4 col-lg-3 ">
-<div class="border border-dark">
-<div class=" text-light text-center mb-2   bg-dark" >
- <h4> <?php echo $Correo ?> </h4>
-<div class=" text-center p-4 bg-light text-dark">
-<p>  <?php  echo   $Nombre ?></p>
-<p> <?php  echo   $Apellidopat ?> </p>
-<p> <?php  echo   $Apellidomat ?> </p>
-</div>
-
-</div>
-</div>
-</div>
-<?php } ?>
-
 		</div>
-	</div>
-	<div class="container">
-	<div class="footer" style="color:#fff; background:#34495E;">
-				<p style="text-align: center;"> © 2019 Copyright: Ahuacatlán, Nayarit.</p>
+	
+		<div class="container mb-2">
+			<h1 class="text-center">
+				Usuarios Existentes
+			</h1>
+			<div class="row">
+
+				<table>
+					<tr>
+					  <td>
+					  	<strong>Nombre(s)</strong>
+					  </td>
+					  
+					  <td>
+					  	<strong>Apellido Paterno</strong>
+					  </td>
+					  
+					  <td>
+					  	<strong>Apellido Materno</strong>
+					  </td>
+
+					  <td>
+					  	<strong>Correo electr&oacute;nico</strong>
+					  </td>
+					</tr>
+
+
+					<?php
+					include("conexion.php");
+
+	  				$cadena='SELECT *FROM usuarios';
+	    			$gsent = $conn->prepare($cadena);
+	    			$gsent->execute();
+	    			while ($resultado = $gsent->fetch(PDO::FETCH_ASSOC)) 
+	    			{
+					    $Correo=$resultado['correoelectronico'];
+					    $Nombre=$resultado['nombre'];
+					    $Apellidopat=$resultado['apellidopaterno'];
+					    $Apellidomat=$resultado['apellidomaterno'];
+					?>
+					
+						<tr>
+							<td>
+						  		<?php  echo   $Nombre ?>
+						  	</td>
+						  	<td>
+						  		<?php  echo   $Apellidopat ?> 
+						  	</td>
+						  	<td>
+						  		<?php  echo   $Apellidomat ?>
+						  	</td>
+						  	<td>
+						  		<?php echo $Correo ?>
+						 	</td>
+						</tr>
+					<?php 
+					} 
+					?>
+				</table>
 			</div>
-	</div>
+		</div>
+		<div class="container">
+			<div class="footer" style="color:#fff; background:#34495E;">
+				<p style="text-align: center;"> 
+					© 2019 Copyright: Ahuacatlán, Nayarit.
+				</p>
+			</div>
+		</div>
 	</body>
 </html>
