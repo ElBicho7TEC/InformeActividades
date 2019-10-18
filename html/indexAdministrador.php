6ffa1fddcd904c1f603439310c7beeb1d57f5076
Ramon Diaz <ramondiaz123@yahoo.com>
2019-10-17 23:10:23 -0600

    Se puede buscar por fecha los informes del amdmin.

--- html/indexAdministrador.php
+++ html/indexAdministrador.php
@@ -36,13 +36,34 @@
 
   
     </select>
-    <input type="submit" id="Guardar" name="Buscar" value="Buscar">
+    <input type="submit" id="BuscarDependencia" name="BuscarDependencia" value="Buscar por Dependencia">
     </div>
     <!-- Fin del tipo -->
 
   </form>
 </div>
 
+<div class="col-12 text-center  ">
+    <b>Busqueda por fecha:</b>
+  <form action="indexAdministrador.php"  method="post"  enctype="multipart/form-data">
+    <!-- Fechas -->
+	 <div class="form-group text-center">
+						  <span>Fecha Inicial:</span>
+						  <input type="date" id="FechaInicio" name="FechaInicio" required=""  maxlength="45">
+					   </div>
+					    <div class="form-group text-center">
+						  <span>Fecha Final:</span>
+						  <input type="date" id="FechaFinal" name="FechaFinal" required=""  maxlength="45">
+					   </div>
+    <div class="form-group">
+ <!--Fin Fechas -->
+ 
+    <input type="submit" id="BuscarFecha" name="BuscarFecha" value="Buscar por Fecha">
+    </div>
+
+
+  </form>
+</div>
 
 
 <form class="Myformulario"enctype='multipart/form-data' method="POST" action="generarinformeAdminDB.php">
@@ -92,12 +113,24 @@
 			$cadena = $cadena. " WHERE actividad.idactividad=historial_informe.idactividad  and  usuarios.iddependencia=actividad.iddependencia
 			and dependencia.iddependencia=actividad.iddependencia and historial_informe.idinforme=informe.idinforme and informe.idusuario=usuarios.idusuario ";
 			
+			//Busqueda por dependencia
 			if(isset($_POST["dependencia"])){
 			$idDependenciaSeleccionada=$_POST['dependencia'];
 			
 			$cadena=$cadena." and actividad.iddependencia=$idDependenciaSeleccionada " ;	
 
 			}
+			//Fin Busqueda por dependencia
+			
+			//Busqueda por fecha
+			if(isset($_POST["FechaInicio"]) and isset($_POST["FechaFinal"])){
+			$FechaInicio=$_POST['FechaInicio'];
+			$FechaFinal=$_POST['FechaFinal'];
+			
+			$cadena=$cadena." and  informe.fecha  between  '$FechaInicio' and '$FechaFinal' " ;	
+
+			}
+			//Fin Busqueda por fecha
 			
 			//order by
 			$cadena = $cadena." ORDER BY  informe.idinforme DESC;" ;
